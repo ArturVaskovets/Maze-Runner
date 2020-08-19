840cff385eda1edcdead6c861aedeab0447bc15e
ArturVaskovets <artik2607@gmail.com>
2020-08-19 18:21:18 +0200

    Added shop prototype
    Circles, mazes, themese and power ups sections.
    Simple horizontal scroll view.

---
+++ Assets/Scripts/ShopController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    public void SetActiveAllChildrens(bool value)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(value);
+        }
+    }
+}
